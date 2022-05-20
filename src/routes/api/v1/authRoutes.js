8c149f2d8716b4267fadd0f861e0bbb7551046d1
treasure567 <52765128+treasure567@users.noreply.github.com>
2022-05-20 20:56:24 +0100

    new: moved all authentication routes to a new route file

---
+++ src/routes/api/v1/authRoutes.js
@@ -0,0 +1,27 @@
+const router = require("express").Router();
+
+const authController = require("../../../controllers/authController");
+
+module.exports = app => {
+    /* Creating a route for the loginUser function in the userController.js file. */
+    router.post("/auth/login", userController.loginUser);
+
+    /* This is a route that is used to reset the password of a user. */
+    router.post("/auth/reset-password", userController.resetPassword);
+
+    /* This is a route that is used to send a password reset link to the user's email address. */
+    router.post("/auth/forgot-password", userController.forgotPassword);
+
+    /* Telling the server to use the router for the api/v1 route. */
+    app.use('/api/v1', router);
+
+    // error handler
+    /* This is a middleware that is used to catch any errors that may occur in the application. */
+    app.use((err, req, res, next) => {
+        /* This is a middleware that is used to catch any errors that may occur in the application. */
+        res.status(err.statusCode || 500).send({
+            message: err.message
+        });
+        next();
+    });
+};
