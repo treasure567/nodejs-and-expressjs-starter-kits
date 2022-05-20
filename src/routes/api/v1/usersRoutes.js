e32afb184c3185c71545b1d48ca63eae1fd63f1c
treasure567 <52765128+treasure567@users.noreply.github.com>
2022-05-20 20:56:48 +0100

    update: removed auth routes to a new route controller

--- src/routes/api/v1/usersRoutes.js
+++ src/routes/api/v1/usersRoutes.js
@@ -11,18 +11,8 @@ module.exports = app => {
     /* Creating a new user. */
     router.post("/auth/register", userController.createUser);
 
-    /* Creating a route for the loginUser function in the userController.js file. */
-    router.post("/auth/login", userController.loginUser);
-
-
     /* A route that is used to find all the users in the database. */
     router.get("/", userController.findAll);
-
-    /* This is a route that is used to reset the password of a user. */
-    router.post("/auth/reset-password", userController.resetPassword);
-
-    /* This is a route that is used to send a password reset link to the user's email address. */
-    router.post("/auth/forgot-password", userController.forgotPassword);
 
     /* Telling the server to use the router for the api/v1 route. */
     app.use('/api/v1', router);
