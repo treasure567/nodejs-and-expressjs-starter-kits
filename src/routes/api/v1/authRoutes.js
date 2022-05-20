fa79ac9b7571f407317e473e483496e93a3d0071
treasure567 <52765128+treasure567@users.noreply.github.com>
2022-05-20 21:30:49 +0100

    Update authRoutes.js

--- src/routes/api/v1/authRoutes.js
+++ src/routes/api/v1/authRoutes.js
@@ -4,13 +4,13 @@ const authController = require("../../../controllers/authController");
 
 module.exports = app => {
     /* Creating a route for the loginUser function in the userController.js file. */
-    router.post("/auth/login", userController.loginUser);
+    router.post("/auth/login", authController.loginUser);
 
     /* This is a route that is used to reset the password of a user. */
-    router.post("/auth/reset-password", userController.resetPassword);
+    router.post("/auth/reset-password", authController.resetPassword);
 
     /* This is a route that is used to send a password reset link to the user's email address. */
-    router.post("/auth/forgot-password", userController.forgotPassword);
+    router.post("/auth/forgot-password", authController.forgotPassword);
 
     /* Telling the server to use the router for the api/v1 route. */
     app.use('/api/v1', router);
